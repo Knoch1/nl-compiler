6dcc91c37a5187fb5d6fc32a86e2fc31fb01909f
Moritz Prünster <moritzpruenster@tt-consulting.com>
2025-05-15 07:46:47 +0200

    remove class if needed

--- src/steps/del-Link.js
+++ src/steps/del-Link.js
@@ -183,6 +183,17 @@ function convertDivs($el) {
 
 $('.row').removeClass('row');
 $('.column').removeClass('column');
+function removeEmptyClassAttr($el) {
+  const classAttr = $el.attr('class');
+  if (!classAttr || !classAttr.trim()) {
+    $el.removeAttr('class');
+  }
+}
+
+$('table, td').each((_, el) => {
+  removeEmptyClassAttr($(el));
+});
+
   $("tr").attr(
     "style",
     "padding: 0px; margin: 0px; border-spacing: 0; font-size:0; mso-line-height-alt:0; mso-margin-top-alt:1px;"
